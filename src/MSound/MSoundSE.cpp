d7551be4a07c42495ddcf44be3a24fa8c9988995
Matthew Toro <matthewtoro@live.com>
2026-08-17 12:36:08 -0400

    rework(sound): restore random volume calculation flow

--- src/MSound/MSoundSE.cpp
+++ src/MSound/MSoundSE.cpp
@@ -49,15 +49,13 @@ u32 MSRandVol::getRandomVolume(u32 param_1, u32 param_2) { }
 
 f32 MSRandVol::getRandVol(u32 param_1)
 {
-	f32 d
-	    = JALCalc::getRandom(unk3C[param_1 >> 26 & 3] * unk18,
-	                         unk2C[param_1 >> 24 & 3], unk1C[param_1 >> 22 & 3])
-	      + 1.0f;
+	f32 step    = unk1C[param_1 >> 22 & 3];
+	f32 maximum = unk2C[param_1 >> 24 & 3];
+	f32 volume  = unk3C[param_1 >> 26 & 3] * unk18;
+	f32 d       = JALCalc::getRandom(volume, maximum, step) + 1.0f;
 
-	if (d < 0.0f)
-		return 0.0f;
-	if (d > 2.0f)
-		return 2.0f;
+	d = d < 0.0f ? 0.0f : d;
+	d = d > 2.0f ? 2.0f : d;
 	return d;
 }
 
