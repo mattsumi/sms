b9a318173fc60a2c3877e09ea754df2b6f9cd802
Matthew Toro <matthewtoro@live.com>
2026-08-17 14:47:23 -0400

    feature(enemy): restore igaiga rolling sound

--- src/Enemy/igaiga.cpp
+++ src/Enemy/igaiga.cpp
@@ -1,5 +1,6 @@
 #include <Enemy/Igaiga.hpp>
 #include <Player/MarioAccess.hpp>
+#include <MSound/MSound.hpp>
 
 void TRollEnemy::attackToMario()
 {
@@ -9,6 +10,13 @@ void TRollEnemy::attackToMario()
 void TIgaiga::perform(u32 cue, JDrama::TGraphics* graphics)
 {
 	TSmallEnemy::perform(cue, graphics);
+}
+
+void TIgaiga::rollSE()
+{
+	MSound* sound = gpMSound;
+	sound->startSoundActorSpecial(MSD_SE_EN_IGAIGA_ROLL, &mPosition, mScaling.x,
+	                              mMarchSpeed, 0, nullptr, 0, 4);
 }
 
 void TIgaiga::setWalkAnm() { setBckAnm(3); }
