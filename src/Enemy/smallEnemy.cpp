caa5d8e75dc67e6adda075e48d0560f6067c85cc
Matthew Toro <matthewtoro@live.com>
2026-08-19 03:09:51 -0400

    rework(enemy): retain small enemy item roll range

--- src/Enemy/smallEnemy.cpp
+++ src/Enemy/smallEnemy.cpp
@@ -429,8 +429,8 @@ void TSmallEnemy::setAfterDeadEffect()
 
 void TSmallEnemy::generateItem()
 {
-	f32 rand = TMsRange<f32>(0.0f, 100.0f).rand();
-	(void)rand; // TODO: due to rand being incorrect
+	TMsRange<f32> itemRollRange(0.0f, 100.0f);
+	f32 rand     = itemRollRange.rand();
 	f32 eggRate  = getSaveParams()->mSLGenEggRate.get();
 	f32 itemRate = getSaveParams()->mSLGenItemRate.get();
 	if (rand < eggRate + itemRate && !mGroundPlane->isIllegalData())
